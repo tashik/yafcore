610ee92c5d09353342a5ee357553be3fc8d29c75
Grundik <grundik@ololo.cc>
2013-01-07 14:07:05 +0300

    Fix

--- application/framework/Zend.php
+++ application/framework/Zend.php
@@ -3,7 +3,11 @@
 require_once 'Zend/Loader/Autoloader.php';
 $autoloader = Zend_Loader_Autoloader::getInstance();
 
-class My_Application extends Zend_Application {}
+class My_Application extends Zend_Application {
+  public function __construct($options, $unused = null) {
+    parent::__construct(APPLICATION_ENV, $options);
+  }
+}
 class My_Bootstrap_Abstract extends Zend_Application_Bootstrap_Bootstrap {}
 class My_Controller_Abstract extends Zend_Controller_Action {}
 class My_Plugin_Abstract extends Zend_Controller_Plugin_Abstract {}
